ee0d49ae46321960adacc054d041377622877b10
Philipp Engelmann <pengelmann92@googlemail.com>
2019-01-15 21:38:59 +0100

    Remove unused code

--- src/main.py
+++ src/main.py
@@ -7,14 +7,6 @@ import argparse
 def main():
     game_args = parse_arguments()
     play(game_args)
-
-
-def run_scenario(scenario):
-    if scenario == 'Basic':
-        run_basic()
-    elif scenario == 'Deadly_Corridor':
-        run_deadly_corridor()
-
         
 if __name__ == '__main__':
     main()
